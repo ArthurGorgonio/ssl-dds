2e72bf7bf36f9be9a2d996fade5141b477c09f2d
arthurgorgonio <gorgonioarthur@gmail.com>
2019-09-17 01:59:26 -0300

    Rename files names

---
+++ src/database.R
@@ -0,0 +1,35 @@
+#' @description This function get a batch with n samples of a data stream.
+#'
+#' @param database the data frame (data stream).
+#' @param n the number of the samples which be select to compose a batch.
+#'
+#' @return a batch with n samples and all features and class of the data set.
+#'
+getBatch <- function(database, n) {
+  return(database$get_points(n))
+}
+
+#' @description Read a data set from a arff file
+#'
+#' @param datasetName The name of the data set which be read.
+#' @param fold The fold location on the computer.
+#'
+#' @return The data set loaded in memory.
+#'
+getDatabase <- function(datasetName, fold = "../datasets") {
+  database <- read.arff(paste(fold, datasetName, sep = "/"))
+  return(database)
+}
+
+
+#' @description This function read a data stream and transform to spetial type
+#'  of the dataframe.
+#'
+#' @param dbName the name of the data stream (data base).
+#'
+#' @return an object of the `datastream_dataframe` class.
+#'
+readData <- function(dbName) {
+  data <- datastream_dataframe(factorise(getDatabase(dbName)))
+  return(data)
+}
--- src/databese.R
+++
@@ -1,8 +0,0 @@
-getBatch <- function(database, n) {
-  return(database$get_points(n))
-}
-
-readData <- function(dbName) {
-  data <- factorise(datastream_dataframe(dbName))
-  return (data)
-}
