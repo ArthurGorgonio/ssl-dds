0bade65efffcfaf37f97d25b4a205546f20840e5
Arthur Gorgonio <gorgonioarthur@gmail.com>
2019-09-18 10:38:29 -0300

    Adding new packages functions

--- src/utils.R
+++ src/utils.R
@@ -1,11 +1,14 @@
 #' @description Install packages if it was not installed and load them.
 #'
 installNeedPacks <- function() {
-  packages <- c("ssc", "plyr", "DMwR", "DMwR2", "RWeka", "rminer", "e1071",
-                "ggplot2", "PMCMR", "RMOA")
+  packages <- c("plyr", "DMwR", "DMwR2", "RWeka", "rminer", "e1071", "ggplot2",
+                "PMCMR", "RMOA", "ssc", "scmamp")
+  if(!require("BiocManager")) {
+    install.packages("BiocManager")
+  }
   for (pack in packages) {
     if (!require(pack, character.only = TRUE)) {
-      install.packages(pack)
+      BiocManager::install(pack)
     }
     library(pack, character.only = TRUE, verbose = F)
   }
