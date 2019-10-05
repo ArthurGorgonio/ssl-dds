61ac0fd8f15b5b5b264727cec2509b5d54af0f03
ArthurGorgonio <gorgonioarthur@gmail.com>
2019-10-05 20:07:03 -0300

    improuve variables names. And choose the best model to compose the ensemble

--- main.R
+++ main.R
@@ -11,7 +11,7 @@ setWorkspace <- function() {
 # seeds <- c(1, 49, 31, 435, 14134, 3431, 314, 7, 8999, 559)
 
 shuffleClassify <- function(size) {
-  typeClassify <- 1:length(obj)
+  typeClassify <- 1:length(baseClassifiers)
   return(sample(typeClassify, size, T))
 }
 
@@ -29,13 +29,18 @@ set.seed(1)
 # # DEFINES!!
 # iniLab <- 1
 # dataset <- databases[1]
-# learner <- myLearner[[1]]
+# learner <- classifier[[1]]
 # fold <- folds[[1]]
-# predFunc <- myFuncs[match(list(learner), myLearner)]
+# predFunc <- myFuncs[match(list(learner), classifier)]
 # method <- "1"
 # sup <- labelIds
 # classiName <- learner@func
 # cr <- 5
+#'
+#' Camp fire
+#' Frost fall
+#' iNeed
+#'
 # ##
 for (iniLab in 1:5) {
   ratio <- iniLab * 0.05
@@ -43,24 +48,21 @@ for (iniLab in 1:5) {
     originalDB <- readData(dataset)
     while (!(originalDB$finished)) {
       if (originalDB$processed == 0) {
-        classify <- shuffleClassify(3)
+        typeClassifier <- shuffleClassify(3)
       } else {
-        classify <- shuffleClassify(1)
+        typeClassifier <- shuffleClassify(1)
       }
-      myLearner <- obj[classify]
-      myFuncs <- funcType[classify]
+      classifier <- baseClassifiers[typeClassifier]
+      myFuncs <- funcType[typeClassifier]
       dataL <- getBatch(originalDB, 500)
-      needUpdate <- which(match(myLearner, obj) == 4)
+      needUpdate <- which(match(classifier, baseClassifiers) == 4)
       if (length(needUpdate)) {
-        myLearner[needUpdate] <- attKValue(dataL)
+        classifier[needUpdate] <- attKValue(dataL)
       }
-      #'
-      #' Camp fire
-      #' Frost fall
-      #' iNeed
-      #'
       folds <- stratifiedKFold(dataL, dataL$class)
-      for (learner in myLearner) {
+      for (learner in classifier) {
+        trainedModels <- c()
+        accuracy <- c()
         for (fold in folds) {
           train <- dataL[-fold, ]
           test <- dataL[fold, ]
@@ -70,23 +72,24 @@ for (iniLab in 1:5) {
           classDist <- ddply(data[labelIds, ], ~class, summarise,
                              samplesClass = length(class))
           initialAcc <- supAcc(learner@func, data[labelIds, ])
-          model <- flexConC(learner, myFuncs[match(list(learner), myLearner)],
+          model <- flexConC(learner, myFuncs[match(list(learner), classifier)],
                             classDist, initialAcc, "1", data, labelIds,
                             learner@func, 5)
           modelSup <- supModel(learner@func, train)
-          confusionMatrix(model, test)  
-          confusionMatrix(modelSup, test)  
-          getAcc(confusionMatrix(model, test))
-          getAcc(confusionMatrix(modelSup, test))
+          trainedModels <- c(trainedModels, list(model))
+          accuracy <- c(accuracy, getAcc(confusionMatrix(model, test)))
         }
+        ensemble <- theBestModel(ensemble, trainedModels, accuracy)
         #' TODO below checklist:
         #'  [X] Split the `train` into label and unlabel sets.
         #'  [X] Call training script to create the ensemble (3 instances).
         #'  [?] Add more each iteration of the method, if needed.
-        #'  [ ] Also meansure the accuracy of each individual classifier.
+        #'  [X] Also meansure the accuracy of each individual classifier.
+        #'  [X] Storage all ten model per learner.
+        #'  [X] Measure the best model, using Acc.
         #'  [ ] Compare with oracle ensemble member.
-        #'  [ ] Storage all ten model per learner.
-        #'  [ ] Measure the best model, using Acc.
+        #'  [ ] Join the models in the ensemble list.
+        #'  [ ] 
       }
     }
   }
