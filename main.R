a271d629943c30b1d66f2bae0b9a7d7f585e9920
Arthur Gorgônio <gorgonioarthur@gmail.com>
2020-01-25 12:09:36 -0300

    fixes in supervised branch

--- main.R
+++ main.R
@@ -28,102 +28,104 @@ for (scri in scripts) {
   source(scri)
 }
 rm(scripts, scri)
-defines()
 meansFlexConC1S <- c()
 meansFlexConC1V <- c()
 databases <- list.files(path = "../datasets")
 ratio <- 0.05
-dataLength <- 50000
-for (dataset in databases) {
-  dataName <- strsplit(dataset, ".", T)[[1]][1]
-  cat(dataName)
-  for (seed in seeds) {
-    set.seed(seed)
-    originalDB <- readData(dataset)
-    epoch <- 0
-    ensemble <- list()
-    while ((nrow(originalDB$data)) > (originalDB$state)) {
-      epoch <- epoch + 1
-      if (originalDB$processed == 0) {
-        typeClassifier <- shuffleClassify(3)
-      } else {
-        typeClassifier <- shuffleClassify(1)
-      }
-      allDataL <- getBatch(originalDB, dataLength)
-      allDataL$class <- droplevels(allDataL$class)
-      begin <- Sys.time()
-      dataL <- holdout(allDataL$class, .75)
-      dataTrain <- allDataL[dataL$tr, ]
-      dataTest <- allDataL[dataL$ts, ]
-      rownames(dataTrain) <- as.character(1:nrow(dataTrain))
-      classifier <- baseClassifiers[typeClassifier]
-      myFuncs <- funcType[typeClassifier]
-      needUpdate <- which(typeClassifier == 4)
-      if (length(needUpdate)) {
-        classifier[needUpdate] <- attKValue(dataTrain)
-      }
-      folds <- stratifiedKFold(dataTrain, dataTrain$class)
-      for (learner in classifier) {
-        trainedModels <- c()
-        accFold <- c()
-        fmeasureFold <- c()
-        precisionFold <- c()
-        recallFold <- c()
-        accTest <- c()
-        fmeasureTest <- c()
-        precisionTest <- c()
-        recallTest <- c()
-        for (fold in folds) {
-          train <- dataTrain[-fold, ]
-          test <- dataTrain[fold, ]
-          trainIds <- holdout(train$class, ratio)
-          labelIds <- trainIds$tr
-          data <- newBase(train, labelIds)
-          classDist <- ddply(data[labelIds, ], ~class, summarise,
-                             samplesClass = length(class))
-          initialAcc <- supAcc(learner@func, data[labelIds, ])
-          model <- flexConC(learner, myFuncs[match(list(learner), classifier)],
-                            classDist, initialAcc, "1", data, labelIds,
-                            learner@func, 5)
-          trainedModels[[length(trainedModels) + 1]] <- model
-          cmFold <- confusionMatrix(model, test)
-          cat("\n\tCM FOLD:\n")
-          print(cmFold)
-          accFold <- c(accFold, getAcc(cmFold))
-          fmeasureFold <- c(fmeasureFold, fmeasure(cmFold))
-          precisionFold <- c(precisionFold, precision(cmFold))
-          recallFold <- c(recallFold, recall(cmFold))
-          cmTest <- confusionMatrix(model, dataTest)
-          cat("\n\tCM TEST:\n")
-          print(cmTest)
-          accTest <- c(accTest, getAcc(cmTest))
-          fmeasureTest <- c(fmeasureTest, fmeasure(cmTest))
-          precisionTest <- c(precisionTest, precision(cmTest))
-          recallTest <- c(recallTest, recall(cmTest))
-        }
-        end <- Sys.time()
-        fileName <- paste(dataName, dataLength, ".txt", sep = "")
-        writeArchive(paste("FOLD", fileName, sep = ""), "../results/", dataName,
-                     accFold, fmeasureFold, precisionFold, recallFold, begin,
-                     end, epoch)
-        writeArchive(paste("TEST", fileName, sep = ""), "../results/", dataName,
-                     accTest, fmeasureTest, precisionTest, recallTest,
-                     begin, end, epoch)
-        if (epoch > 1) {
-          bestOracle <- trainedModels[[which.max(accTest)]]
-          realClass <- dataTrain$class
-          dataTrain$class <- predictClass(bestOracle, dataTrain)
-          ensemblePred <- predictEnsemble(ensemble, dataTrain, nrow(classDist))
-          acc <- getAcc(table(ensemblePred, dataTrain$class))
-          if (acc < 70) {
-            clAcc <- measureEnsemble(ensemble, dataTrain)
-            ensemble <- swapEnsemble(ensemble, dataTrain, trainedModels, accTest)
-            changed <- T
-          } else {
-            changed <- F
-          }
+lengthBatch <- c(500, 5000)
+for (dataLength in lengthBatch) {
+  defines()
+  for (dataset in databases) {
+    dataName <- strsplit(dataset, ".", T)[[1]][1]
+    cat(dataName)
+    for (seed in seeds) {
+      set.seed(seed)
+      originalDB <- readData(dataset)
+      epoch <- 0
+      ensemble <- list()
+      while ((nrow(originalDB$data)) > (originalDB$state)) {
+        epoch <- epoch + 1
+        if (originalDB$processed == 0) {
+          typeClassifier <- shuffleClassify(3)
         } else {
-          ensemble <- addingEnsemble(ensemble, trainedModels, accTest)
+          typeClassifier <- shuffleClassify(1)
+        }
+        allDataL <- getBatch(originalDB, dataLength)
+        allDataL$class <- droplevels(allDataL$class)
+        begin <- Sys.time()
+        dataL <- holdout(allDataL$class, .75)
+        dataTrain <- allDataL[dataL$tr, ]
+        dataTest <- allDataL[dataL$ts, ]
+        rownames(dataTrain) <- as.character(1:nrow(dataTrain))
+        classifier <- baseClassifiers[typeClassifier]
+        myFuncs <- funcType[typeClassifier]
+        needUpdate <- which(typeClassifier == 4)
+        if (length(needUpdate)) {
+          classifier[needUpdate] <- attKValue(dataTrain)
+        }
+        folds <- stratifiedKFold(dataTrain, dataTrain$class)
+        for (learner in classifier) {
+          trainedModels <- c()
+          accFold <- c()
+          fmeasureFold <- c()
+          precisionFold <- c()
+          recallFold <- c()
+          accTest <- c()
+          fmeasureTest <- c()
+          precisionTest <- c()
+          recallTest <- c()
+          for (fold in folds) {
+            train <- dataTrain[-fold, ]
+            test <- dataTrain[fold, ]
+            trainIds <- holdout(train$class, ratio)
+            labelIds <- trainIds$tr
+            data <- newBase(train, labelIds)
+            classDist <- ddply(data[labelIds, ], ~class, summarise,
+                               samplesClass = length(class))
+            initialAcc <- supAcc(learner@func, data[labelIds, ])
+            model <- flexConC(learner, myFuncs[match(list(learner), classifier)],
+                              classDist, initialAcc, "1", data, labelIds,
+                              learner@func, 5)
+            trainedModels[[length(trainedModels) + 1]] <- model
+            cmFold <- confusionMatrix(model, test)
+            cat("\n\tCM FOLD:\n")
+            print(cmFold)
+            accFold <- c(accFold, getAcc(cmFold))
+            fmeasureFold <- c(fmeasureFold, fmeasure(cmFold))
+            precisionFold <- c(precisionFold, precision(cmFold))
+            recallFold <- c(recallFold, recall(cmFold))
+            cmTest <- confusionMatrix(model, dataTest)
+            cat("\n\tCM TEST:\n")
+            print(cmTest)
+            accTest <- c(accTest, getAcc(cmTest))
+            fmeasureTest <- c(fmeasureTest, fmeasure(cmTest))
+            precisionTest <- c(precisionTest, precision(cmTest))
+            recallTest <- c(recallTest, recall(cmTest))
+          }
+          end <- Sys.time()
+          fileName <- paste(dataName, dataLength, ".txt", sep = "")
+          writeArchive(paste("FOLD", fileName, sep = ""), "../results/", dataName,
+                       accFold, fmeasureFold, precisionFold, recallFold, begin,
+                       end, epoch)
+          writeArchive(paste("TEST", fileName, sep = ""), "../results/", dataName,
+                       accTest, fmeasureTest, precisionTest, recallTest,
+                       begin, end, epoch)
+          if (epoch > 1) {
+            bestOracle <- trainedModels[[which.max(accTest)]]
+            realClass <- dataTrain$class
+            dataTrain$class <- predictClass(bestOracle, dataTrain)
+            ensemblePred <- predictEnsemble(ensemble, dataTrain, nrow(classDist))
+            acc <- getAcc(table(ensemblePred, dataTrain$class))
+            if (acc < 70) {
+              clAcc <- measureEnsemble(ensemble, dataTrain)
+              ensemble <- swapEnsemble(ensemble, dataTrain, trainedModels, accTest)
+              changed <- T
+            } else {
+              changed <- F
+            }
+          } else {
+            ensemble <- addingEnsemble(ensemble, trainedModels, accTest)
+          }
         }
       }
     }
--- src/statistics.R
+++ src/statistics.R
@@ -1,3 +1,20 @@
+fixCM <- function(cm) {
+  if (nrow(cm) < ncol(cm)) {
+    truePos <- match(rownames(cm), colnames(cm))
+    newCM <- matrix(rep(0,2*ncol(cm)), nrow = ncol(cm), ncol = ncol(cm))
+    colnames(newCM) <- colnames(cm)
+    rownames(newCM) <- colnames(cm)
+    newCM[truePos,] <- cm
+  } else {
+    truePos <- match(colnames(cm), rownames(cm))
+    newCM <- matrix(rep(0,2*nrow(cm)), nrow = nrow(cm), ncol = nrow(cm))
+    colnames(newCM) <- rownames(cm)
+    rownames(newCM) <- rownames(cm)
+    newCM[, truePos] <- cm
+  }
+  return(newCM)
+}
+
 #' @description Calculate the acc of the main diagonal of the matrix.
 #'
 #' @param matrix The confusion matrix of a model.
@@ -18,8 +35,12 @@ getAcc <- function(matrix) {
 #'
 precision <- function(cm) {
   preci <- c()
-  for (i in 1:nrow(cm)) {
-    tp <- cm[i, i]
+  for (i in 1:ncol(cm)) {
+    tryCatch({
+      tp = cm[i, i]
+    }, error = function(e) {
+      tp = 0
+    })
     fp <- sum(cm[i,]) - tp
     if (!((fp == 0) && (tp == 0))) {
       preci <- c(preci, (tp / (tp + fp)))
@@ -27,7 +48,7 @@ precision <- function(cm) {
       preci <- c(preci, 0)
     }
   }
-  return(sum(preci) / nrow(cm))
+  return(mean(preci))
 }
 
 #' @description Calculate the recall using the confusion matrix.
@@ -38,12 +59,21 @@ precision <- function(cm) {
 #'
 recall <- function(cm) {
   recal <- c()
-  for (i in 1:nrow(cm)) {
-    tp <- cm[i, i]
+  for (i in 1:ncol(cm)) {
+    tryCatch({
+        tp = cm[i, i]
+      }, error = function(e){
+        tp = 0
+      }
+    )
     fn <- sum(cm[,i]) - tp
-    recal <- c(recal, (tp / (tp + fn)))
+    if (!((fn == 0) && (tp == 0))) {
+      recal <- c(recal, (tp / (tp + fn)))
+    } else {
+      recal <- c(recal, 0)
+    }
   }
-  return(sum(recal) / nrow(cm))
+  return(mean(recal))
 }
 
 #' @description Calculate the f-measure using the confusion matrix.
