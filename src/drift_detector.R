e14da923d16d592dec933b604f331d7c640c7c91
ArthurGorgonio <gorgonioarthur@gmail.com>
2022-02-20 18:41:48 -0300

    Change the class package

--- src/drift_detector.R
+++ src/drift_detector.R
@@ -8,80 +8,98 @@
 #' @param lambda default value is 50
 #' @param alpha default value is 1 - 0.0001 = 0.9999
 #'  
-page_hinkley <- setRefClass("page_hinkley",
-                            fields = list(delta_='numeric', lambda_='numeric',
-                                        alpha_='numeric', sum_='numeric',
-                                        x_mean_='numeric', num_='numeric',
-                                        change_detected='logical'),
-                            methods = list(
-                              initialize = function(..., delta=0.005, lambda=50,
-                                                    alpha=0.9999) {
-                                delta_ <<- delta
-                                lambda_ <<- lambda
-                                alpha_ <<- alpha
-                                sum_ <<- 0
-                                x_mean_ <<- 0
-                                num_ <<- 0
-                                change_detected <<- FALSE
-                              },
-                              .detect_drift = function(x) {
-                                num_ <<- num_ + 1
-                                x_mean_ <<- (x + x_mean_ * (num_ - 1)) / num_
-                                sum_ <<- sum_ * alpha_ + (x - x_mean_ - delta_)
-                                if (sum_ > lambda_) {
-                                  change_detected <<- TRUE
-                                  .reset_params()
-                                } else {
-                                  change_detected <<- FALSE
-                                }
-                                return (change_detected)
-                              },
-                              .reset_params = function() {
-                                num_ <<- 0
-                                x_mean_ <<- 0
-                                sum_ <<- 0
-                              },
-                              set_input = function(x) {
-                                return(.detect_drift(x))
-                              }
-                            )
+PageHinkley <- R6Class('PageHinkley',
+                        private = list(
+                          .delta = 0,
+                          .lambda = 0,
+                          .alpha = 0,
+                          .sum = 0,
+                          .x_mean = 0,
+                          .count = 0
+                        ),
+                        public = list(
+                          initialize = function(delta=0.005, lambda=50,
+                                               alpha=0.9999) {
+                            stopifnot(is.numeric(delta), length(delta) == 1)
+                            stopifnot(is.numeric(lambda), length(lambda) == 1)
+                            stopifnot(is.numeric(alpha), length(alpha) == 1)
+                            private$.delta <- delta
+                            private$.lambda <- lambda
+                            private$.alpha <- alpha
+                          },
+                          detect_drift = function(x) {
+                            private$.count <- private$.count + 1
+                            private$.x_mean <- (x + private$.x_mean
+                                                  * (private$.count - 1))
+                                               / private$.count
+                            private$.sum <- private$.sum * private$.alpha
+                                            + (x - private$.x_mean
+                                               - private$.delta)
+                            if (private$.sum > private$.lambda) {
+                              self$reset_params()
+                              return(TRUE)
+                            }
+                            return(FALSE)
+                          },
+                          reset_params = function() {
+                            private$.sum <- 0
+                            private$.x_mean <- 0
+                            private$.count <- 0
+                          }
+                        )
 )
 
-CPSSDS <- setRefClass('CPSSDS',
-                      fields = list(last_chunk='factor',
-                                    actual_chunk='factor',
-                                    statistical_test='character',
-                                    change_detected='logical'),
-                      methods = list(
-                        initialize = function(...,
-                                              statistical_test='kolmogorov') {
-                          statistical_test <<- statistical_test
-                          change_detected <<- FALSE
+
+CPSSDS <- R6Class('CPSSDS',
+                  private = list(
+                    .last_chunk = NA,
+                    .actual_chunk = NA,
+                    .statistical_test = 'kolmogorov'
+                  ),
+                  public = list(
+                    initialize = function(statistical_test='kolmogorov') {
+                      stopifnot(is.character(statistical_test), 
+                                str_to_lower(statistical_test) %in%
+                                  c('kolmogorov', 't-test'),
+                                length(statistical_test) == 1)
+                      private$.statistical_test <- str_to_lower(statistical_test)
+                    },
+                    eval_test = function() {
+                      switch (private$.statistical_test,
+                        'kolmogorov' = {
+                          test <- ks.test(as.numeric(private$.last_chunk),
+                                          as.numeric(private$.actual_chunk))
                         },
-                        .eval_test = function() {
-                          switch (statistical_test,
-                            'kolmogorov' = {
-                              test <- ks.test(as.numeric(last_chunk),
-                                              as.numeric(actual_chunk))
-                            },
-                            't-test' = {
-                              test <- t.test(as.numeric(last_chunk),
-                                             as.numeric(actual_chunk))
-                            }
-                          )
-                          return(test)
-                        },
-                        detect_drift = function() {
-                          change_detected <<- FALSE
-                          p <- .eval_test()
-                          if (p$p.value < 0.05) {
-                            change_detected <<- TRUE
-                          }
-                          return(change_detected)
-                        },
-                        get_chunk = function(labels) {
-                          last_chunk <<- actual_chunk
-                          actual_chunk <<- labels
+                        't-test' = {
+                          test <- t.test(as.numeric(private$.last_chunk),
+                                         as.numeric(private$.actual_chunk))
                         }
                       )
+                      return(test)
+                    },
+                    detect_drift = function() {
+                      p <- self$eval_test()
+                      if (p$p.value < 0.05) {
+                        return(TRUE)
+                      }
+                      return(FALSE)
+                    },
+                    get_chunk = function(labels) {
+                      private$.last_chunk <- c(private$.actual_chunk)
+                      private$.actual_chunk <- labels
+                    }
+                  )
 )
+
+
+# base <- iris[sample(1:nrow(iris)),]
+
+# all_levels <- sort(levels(base$Species))
+# cpssds <- CPSSDS$new()
+# train <- datastream_dataframe(data = base)
+# data <- getBatch(train, 50)
+# cpssds$get_chunk(data$Species)
+# data <- getBatch(train, 50)
+# data <- getBatch(train, 50)
+# cpssds$get_chunk(data$Species)
+# cat(cpssds$detect_drift())
